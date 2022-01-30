7a10e018dac67eb36e01b66e1e345581a7e87363
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-01-30 18:00:50 -0500

    add debug mode

--- rest/src/Controllers/UserController.cs
+++ rest/src/Controllers/UserController.cs
@@ -6,39 +6,36 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 using rest.src.Models;
 using rest.src.Services;
 
-namespace rest.src.Controllers
+namespace rest.src.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UserController
+    private readonly ILogger<UserController> _logger;
+    private readonly TypicodeService _typicode;
+
+    public UserController(ILogger<UserController> logger, TypicodeService tropiPay)
     {
-        private readonly ILogger<UserController> _logger;
-        private readonly TypicodeService _typicode;
+        _logger = logger;
+        _typicode = tropiPay;
+    }
 
-        public UserController(ILogger<UserController> logger, TypicodeService tropiPay)
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        try
         {
-            _logger = logger;
-            _typicode = tropiPay;
+            var result = await _typicode.GetUsersAsync();
+            return Ok(result);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        catch (HttpRequestException)
         {
-            try
-            {
-                var result = await _typicode.GetUsersAsync();
-                
-                return null;//Ok(result)
-            }
-            catch (HttpRequestException)
-            {
-                return null; //NotFound();
-            }
+            return NotFound();
         }
     }
 }
--- rest/src/Services/TypicodeService.cs
+++ rest/src/Services/TypicodeService.cs
@@ -11,13 +11,15 @@ namespace rest.src.Services
     public class TypicodeService
     {
         private readonly HttpClient _httpClient;
+        private string _url;
         private string _clientId;
         private string _clientSecret;
 
         public TypicodeService(HttpClient httpClient, IConfiguration configuration)
         {
+            _url = "https://jsonplaceholder.typicode.com/";
             _httpClient = httpClient;
-            _httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            _httpClient.BaseAddress = new Uri(_url);
             _httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             _httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "HttpRequestsSample");
 
@@ -25,9 +27,9 @@ namespace rest.src.Services
             _clientSecret = configuration.GetValue<string>("ClientSecret");
         }
 
-        public async Task<User?> GetUsersAsync()
+        public async Task<IList<User>?> GetUsersAsync()
         {
-            return await _httpClient.GetFromJsonAsync<User>("users");
+            return await _httpClient.GetFromJsonAsync<IList<User>>("users");
         }
 
     }
