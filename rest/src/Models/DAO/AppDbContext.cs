034d455099672abf2d1052b89e795fc44578fa8a
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-01-30 19:51:05 -0500

    add migrations

--- rest/src/Models/DAO/AppDbContext.cs
+++ rest/src/Models/DAO/AppDbContext.cs
@@ -4,33 +4,16 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-using rest.src.Models.DTO;
+using rest.src.Models.ORM;
 namespace rest.src.Models.DAO
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        // Constructor to use on a DbConnection that is already opened
-        /*public AppDbContext(DbConnection existingConnection, bool contextOwnsConnection)
-          : base(existingConnection, contextOwnsConnection)
-        {
-
-        }
-
-        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<User>().MapToStoredProcedures();
-            modelBuilder.Entity<Address>().MapToStoredProcedures();
-            modelBuilder.Entity<Company>().MapToStoredProcedures();
-            modelBuilder.Entity<Geopoint>().MapToStoredProcedures();
-        }*/
-
         public DbSet<User> User { get; set; }
         public DbSet<Address> Address { get; set; }
         public DbSet<Company> Company { get; set; }
         public DbSet<Company> Geopoint { get; set; }
-
     }
 }
---
+++ rest/src/Models/ORM/Company.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rest.src.Models.ORM
+{
+    public class Company
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+
+        
+        public ICollection<User> Users { get; set; } 
+    }
+}
